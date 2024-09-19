c6fce51beebd1dd652e60226c8df8ba9c6dde61a
StephenAdedayo <alustephenadedayo@gmail.com>
2024-09-19 23:47:59 +0100

    switchong

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { CiHeart } from 'react-icons/ci'
 
 const Products = ({filteredProducts}) => {
+
+    // const {category, title, image, price } = filteredProducts || {}
+
   return (
-    <div>
+    <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1'>
+
+        {filteredProducts.map(filtered => (
+            <div className='shadow-md relative rounded-md p-2'>
+
+                <div className='flex gap-2'>
+                    <img src={filtered.image} alt="" />
+
+                <div className='space-y-2'>
+               <p>{filtered.category}</p>
+               <p>{filtered.title}</p>
+               <p>{filtered.price}</p>
+                </div>
+                </div>
+         
+         <div className='absolute bottom-2 right-2'>
+            <CiHeart className=''/>
+         </div>
+            </div>
+        ))}
       
     </div>
   )
--- src/products/AllProducts.jsx
+++ src/products/AllProducts.jsx
@@ -1,113 +1,103 @@
-import React, { useEffect, useState } from 'react'
-import Products from '../components/Products'
+import React, { useEffect, useState } from "react";
+import Products from "../components/Products";
 
 const AllProducts = () => {
+  // usestates
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [category, setCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
+  const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
 
-// usestates
-  const [products, setProducts] = useState([])
-  const [filteredProducts, setFilteredProducts] = useState([])
-  const [category, setCategory] = useState('all')
-  const [sortOption, setSortOption] = useState('default')
-  const [error, setError] = useState('')
-  const [search, setSearch] = useState('')
-
-// fucntion to fetch all products
+  // fucntion to fetch all products
   const fetchProducts = async () => {
-
-   try {
-    const response = await fetch(``)
-    if(!response.ok){
-      throw new Error('Error occured while fetching data')
+    try {
+      const response = await fetch(``);
+      if (!response.ok) {
+        throw new Error("Error occured while fetching data");
+      }
+      const data = await response.json();
+      setProducts(data);
+      setFilteredProducts(data);
+    } catch (error) {
+      console.log("error occured", error.message);
+      setError(error.message);
     }
-    const data = await response.json()
-    setProducts(data)
-    setFilteredProducts(data)
-   } catch (error) {
-      console.log('error occured', error.message);
-      setError(error.message)
-      
-   }
-
-
-  }
-
+  };
 
   useEffect(() => {
-    fetchProducts()
-  }, [])
-
+    fetchProducts();
+  }, []);
 
   // function to filter items based on food type
   const filterCategory = (category) => {
-      const filter = category === 'all' ? products : products.filter(product => product.category === category) 
-      setCategory(filter)
-      setFilteredProducts(filter)
-  } 
+    const filter =
+      category === "all"
+        ? products
+        : products.filter((product) => product.category === category);
+    setCategory(filter);
+    setFilteredProducts(filter);
+  };
 
-
-// function to showall products
+  // function to showall products
   const showAllProducts = () => {
-    setCategory('all')
-    setFilteredProducts(products)
-  }
-
+    setCategory("all");
+    setFilteredProducts(products);
+  };
 
   // function to handle sortoption
   const handleSortOption = (option) => {
-    setSortOption(option)
+    setSortOption(option);
 
+    const sortedItems = [...products];
 
+    switch (option) {
+      case "A-Z":
+        sortedItems.sort((a, b) => a.title.localCompare(b.title));
+        break;
+      case "Z-A":
+        sortedItems.sort((a, b) => b.title.localCompare(a.title));
+        break;
+      case "high-low":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      case "low-high":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
 
-    const sortedItems = [...products]
-  
-    switch(option) {
-      case  'A-Z' : 
-      sortedItems.sort((a,b) => a.title.localCompare((b.title)))
-      break
-      case 'Z-A' : 
-      sortedItems.sort((a,b) => b.title.localCompare((a.title)))
-      break
-      case 'high-low' : 
-      sortedItems.sort((a,b) => b.price - a.price)
-      break
-      case 'low-high' :
-      sortedItems.sort((a,b) => a.price - b.price)
-      break
-
-      default : 'default'
-      break
-       
+      default:
+        "default";
+        break;
     }
 
-    setFilteredProducts(sortedItems)
-
-  }
-
+    setFilteredProducts(sortedItems);
+  };
 
   // function to handlesearch value
   const handleSearchChange = (e) => {
-    const value = e.target.value
-    setSearch(value)
+    const value = e.target.value;
+    setSearch(value);
 
+    const filtererd = products.filter((product) =>
+      product.title.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredProducts(filtererd);
+  };
 
-   const filtererd = products.filter(product => product.title.toLowerCase().includes(value.toLowerCase()))
-    setFilteredProducts(filtererd)
-  }
-
-  if(!products) return <div>{error}</div>
-   
+  if (!products) return <div>{error}</div>;
 
   return (
     <>
-    <div className='w-full text-center mb-5 p-4 bg-primaryColor '>
-     <p>Most Popular Items</p>
-    </div>
+      <div className="w-full text-center mb-5 p-4 bg-primaryColor ">
+        <p className="text-lg">Most Popular Items</p>
+      </div>
 
-    <div>
-      <Products filteredProducts = {filteredProducts}/>
-    </div>
+      <div>
+        <Products filteredProducts={filteredProducts} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AllProducts
+export default AllProducts;
